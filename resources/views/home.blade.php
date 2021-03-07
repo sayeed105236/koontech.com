24902d3b117a5bbb74c20dea91495779678c23cc
Abu Sayeed <abusayeed33343536@gmail.com>
2021-03-07 17:54:00 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,481 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>koontech.com</title>
+
+
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" type="text/css">
+
+<!-- Icons -->
+<link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.min.css') }}" type="text/css">
+
+<link type="text/css" href="{{ asset('frontend/css/bootstrap-tagsinput.css') }}" rel="stylesheet">
+
+
+
+
+<link type="text/css" href="{{ asset('frontend/css/jquery.share.css') }}" rel="stylesheet">
+
+<!-- Global style (main) -->
+<link type="text/css" href="{{ asset('frontend/css/active-shop.css?v=1.14') }}" rel="stylesheet" media="screen">
+
+
+
+<link type="text/css" href="{{ asset('frontend/css/main.css?v=1.21') }}" rel="stylesheet">
+
+
+
+<!-- Facebook Chat style -->
+<link href="{{ asset('frontend/css/fb-style.css?v=1.16')}}" rel="stylesheet">
+
+<!--Select2 [ OPTIONAL ]-->
+<link href="{{ asset('plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+
+<!-- color theme -->
+
+
+<!-- Custom style -->
+<link type="text/css" href="{{ asset('frontend/css/custom-style.css') }}" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/font-awesome.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/owl.carousel.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/chosen.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/style.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/color-01.css')}}">
+  </head>
+  <body>
+    <div class="top-navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col">
+                        <ul class="inline-links d-lg-inline-block d-flex justify-content-between support-links">
+
+
+
+                            <li><a href="#"> Welcome to KNTECH !</a></li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-5 text-right d-none d-lg-block">
+                        <ul class="inline-links">
+                          <br>
+                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>marketing@koontech.com</a></li>
+
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="container">
+          <div class="col-md-12">
+
+
+
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+
+
+
+  <div class="container">
+
+
+
+
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="col-md-8">
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ABOUT US</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PRODUCTS
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            NEWS
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            SOLUTION
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PROJECT
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PARTNER
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTACT US</a>
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+  </div>
+</nav>
+</div>
+</div>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" style="height:5px; width:40px; background: transparent #FF0000;" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" style="height:5px; width:40px; background: transparent #FF0000;" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" style="height:5px; width:40px; background: transparent #FF0000;" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" style="height:5px; width:40px; background: transparent #FF0000;" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" style="height:5px; width:40px; background:  transparent #FF0000;" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('frontend/images/slider/1.jpg')}}" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+
+
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item ">
+      <img src="{{asset('frontend/images/slider/2.jpg')}}" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+
+
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+      <img src="{{asset('frontend/images/slider/3.jpg')}}" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+
+
+  </div>
+
+</div>
+
+<div class="container" style="margin-top: 60px;">
+
+
+
+<div class="wrap-products" >
+  <div class="wrap-product-tab tab-style-1">
+    <div class="tab-contents">
+      <div class="row">
+
+
+
+
+
+
+
+
+
+      <div class="col-md-12" >
+      <div class="tab-content-item active" id="digital_1a">
+        <div class="wrap-products slide-carousel owl-carousel style-nav-1 equal-container" data-items="5" data-loop="false" data-nav="true" data-dots="false" data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"4"},"1200":{"items":"5"}}' >
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/1.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNSP-03-IP-Y-D18-BL</span></a>
+              <div class="wrap-price"><span class="product-price">Blue Light Stations</span></div>
+            </div>
+          </div>
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/2.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNZD-60M</span></a>
+              <div class="wrap-price"><span class="product-price">Video help point</span></div>
+            </div>
+          </div>
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/3.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+              <div class="group-flash">
+
+              </div>
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNZD-20W</span></a>
+              <div class="wrap-price"><span class="product-price">School intercom</span></div>
+
+            </div>
+          </div>
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/4.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNZD-11</span></a>
+              <div class="wrap-price"><span class="product-price">SOS Intercom</span></div>
+            </div>
+          </div>
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/5.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNZD-09IPIL</span></a>
+              <div class="wrap-price"><span class="product-price">Passenger Help Point</span></div>
+            </div>
+          </div>
+
+          <div class="product product-style-2 equal-elem ">
+            <div class="product-thumnail">
+              <a href="detail.html" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                <figure><img src="{{asset('frontend/images/products/6.jpg')}}" width="800" height="800" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+              </a>
+
+              <div class="wrap-btn">
+                <a href="#" class="function-link">quick view</a>
+              </div>
+            </div>
+            <div class="product-info">
+              <a href="#" class="product-name"><span>Model: KNZD-05 LS</span></a>
+              <div class="wrap-price"><span class="product-price">Passenger Help Point</span></div>
+
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+      </div>
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<!-- FOOTER -->
+<footer id="footer" class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row cols-xs-space cols-sm-space cols-md-space">
+
+
+                <div class="col-lg-3 offset-xl-1 col-md-4">
+                    <div class="col text-center text-md-left">
+                        <h4 class="heading heading-xs strong-600 mb-2">
+                            {{__('Tags:')}}
+                        </h4>
+
+                    </div>
+                </div>
+
+
+                      <div class="col-md-4">
+                                      <ul class="text-center my-3 my-md-0 social-nav model-2">
+
+                                              <li>
+                                                  <a href="" class="facebook" target="_blank" data-toggle="tooltip" data-original-title="Facebook">
+                                                      <i class="fa fa-facebook"></i>
+                                                  </a>
+                                              </li>
+
+                                              <li>
+                                                  <a href="" class="instagram" target="_blank" data-toggle="tooltip" data-original-title="Instagram">
+                                                      <i class="fa fa-instagram"></i>
+                                                  </a>
+                                              </li>
+
+                                              <li>
+                                                  <a href="" class="twitter" target="_blank" data-toggle="tooltip" data-original-title="Twitter">
+                                                      <i class="fa fa-twitter"></i>
+                                                  </a>
+                                              </li>
+
+                                              <li>
+                                                  <a href="" class="youtube" target="_blank" data-toggle="tooltip" data-original-title="Youtube">
+                                                      <i class="fa fa-youtube"></i>
+                                                  </a>
+                                              </li>
+
+                                              <li>
+                                                  <a href="" class="google-plus" target="_blank" data-toggle="tooltip" data-original-title="Google Plus">
+                                                      <i class="fa fa-google-plus"></i>
+                                                  </a>
+                                              </li>
+
+                                      </ul>
+                                  </div>
+
+
+
+
+                <div class="col-md-4 col-lg-2">
+
+                </div>
+            </div>
+            <br>
+            <br>
+
+        </div>
+    </div>
+
+    <div class="footer-bottom py-3 sct-color-3">
+        <div class="container">
+            <div class="row row-cols-xs-spaced flex flex-items-xs-middle">
+                <div class="col-md-4">
+
+                        <ul class="copy-links no-margin">
+
+
+
+                                <li>
+                                    <a href="">Copyright© Hongkong Koon Technology ltd ALL Rights Reserved- 站长统计</a>
+
+                            </li>
+
+
+
+                        </ul>
+
+                    </div>
+
+                <div class="col-md-4">
+                    <ul class="text-center my-3 my-md-0 social-nav model-2">
+
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center text-md-right">
+                        <ul class="inline-links">
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+<script src="{{asset('frontend/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+<script src="{{asset('frontend/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('frontend/js/jquery.flexslider.js')}}"></script>
+<script src="{{asset('frontend/js/chosen.jquery.min.js')}}"></script>
+<script src="{{asset('frontend/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('frontend/js/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('frontend/js/jquery.sticky.js')}}"></script>
+<script src="{{asset('frontend/js/functions.js')}}"></script>
+
+
+
+  </body>
+</html>
